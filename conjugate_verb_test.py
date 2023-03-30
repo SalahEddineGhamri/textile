9957bfa77338e437df3791127657682d512d4d89
salah eddine ghamri <salaheddine.ghamri@yahoo.com>
2023-03-30 22:22:12 +0200

    30.03.2023

---
+++ conjugate_verb_test.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+verb = sys.argv[1]
+
+
+# Construct the URL to search for the verb
+url = f'https://www.verbformen.com/conjugation/?w={verb}'
+
+# Send a GET request to the website and parse the HTML response
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Find the table containing the verb conjugation
+table = soup.find('table', {'class': 'vtable'})
+
+# Parse the table rows to extract the verb forms
+rows = table.find_all('tr')
+
+# Extract the tense names from the first row of the table
+tenses = [th.text.strip() for th in rows[0].find_all('th')[1:]]
+
+# Extract the pronoun names from the first column of the table
+pronouns = [row.find('th').text.strip() for row in rows[1:]]
+
+# Extract the verb forms for each tense and pronoun
+forms = {}
+for tense, row in zip(tenses, rows[1:]):
+    cols = row.find_all('td')[1:]
+    for pronoun, col in zip(pronouns, cols):
+        form = col.text.strip()
+        forms.setdefault(pronoun, {})[tense] = form
+
+# Print the verb conjugation in a table format
+print(f'{"":>6}{" | ".join(tenses)}')
+for pronoun in pronouns:
+    print(f'{pronoun:>6} | {" | ".join(forms[pronoun].values())}')
